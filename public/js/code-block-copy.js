96f97fea0a058c22461240c6a350fcaf24b7839a
headintheclouduk <bates.oliver@hotmail.co.uk>
2025-05-14 08:14:16 +0100

    Replaced theme baseof.html with layouts/_default/baseof.html

--- public/js/code-block-copy.js
+++ public/js/code-block-copy.js
@@ -1,16 +1,14 @@
-<script>
 document.querySelectorAll("pre").forEach((block) => {
   const button = document.createElement("button");
-    button.innerText = "Copy";
-    button.className = "copy-button";
-    block.appendChild(button);
+  button.innerText = "Copy";
+  button.className = "copy-button";
+  block.appendChild(button);
 
   button.addEventListener("click", () => {
     const code = block.querySelector("code");
     navigator.clipboard.writeText(code.innerText).then(() => {
-        button.innerText = "Copied!";
+      button.innerText = "Copied!";
       setTimeout(() => (button.innerText = "Copy"), 2000);
     });
   });
 });
-</script>
